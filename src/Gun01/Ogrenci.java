bd13853f60bcb766606e812731f467299113f443
Halime Efe <halimeefe@outlook.com>
2023-01-31 17:54:23 +0300

    task tamam

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -4,5 +4,8 @@ public class Ogrenci {
     public static void main(String[] args) {
         System.out.println(" değişiklik yapıldı");
 
+        System.out.println("halime kod ekledi");
+
+
     }
 }
